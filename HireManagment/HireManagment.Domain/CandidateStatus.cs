5245441f2496050c28d9545eb10429597c537e04
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 10:45:19 -0600

    Create CandidateStatus.cs

---
+++ HireManagment/HireManagment.Domain/CandidateStatus.cs
@@ -0,0 +1,14 @@
+﻿using HireManagment.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Domain
+{
+    public class CandidateStatus : BaseDomainEntity
+    {
+        public string? Status { get; set; }
+    }
+}
