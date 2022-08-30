7ab059abe6cf7aebd1762426ff1d4092c87c792b
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 15:00:44 -0600

    Create OpeningDto.cs

---
+++ HireManagment/HireManagment.Application/DTOs/Opening/OpeningDto.cs
@@ -0,0 +1,22 @@
+﻿using HireManagment.Application.DTOs.CompanyEmployee;
+using HireManagment.Domain.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.DTOs.Opening
+{
+    public class OpeningDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateExpiration { get; set; }
+        public OpeningType OpeningType { get; set; }
+
+        public int CompanyEmployeeId { get; set; }
+        public virtual CompanyEmployeeDto? CompanyEmployee { get; set; }
+    }
+}
