5d0ff7636e00a69e25d96ca2d209c7f324a75bf5
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 14:46:06 -0600

    Create CandidateDto.cs

---
+++ HireManagment/HireManagment.Application/DTOs/Candidate/CandidateDto.cs
@@ -0,0 +1,14 @@
+﻿using HireManagment.Application.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.DTOs.Candidate
+{
+    public class CandidateDto : PersonDto
+    {
+
+    }
+}
