cace8a9bcc0cf0019006caa4ac6b36b70e5710aa
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 10:45:25 -0600

    Update Company.cs

--- HireManagment/HireManagment.Domain/Company.cs
+++ HireManagment/HireManagment.Domain/Company.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HireManagment.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace HireManagment.Domain
 {
-    internal class Company
+    public class Company : BaseDomainEntity
     {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Address { get; set; }
     }
 }
