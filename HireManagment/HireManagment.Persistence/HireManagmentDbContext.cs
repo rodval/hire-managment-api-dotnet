989d9e12abe8f1cb94fee1c069f3b1b7270ae58f
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 16:26:05 -0600

    set up persistence project

---
+++ HireManagment/HireManagment.Persistence/HireManagmentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HireManagment.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Persistence
+{
+    public class HireManagmentDbContext : DbContext
+    {
+        public HireManagmentDbContext(DbContextOptions<HireManagmentDbContext> options)
+            : base(options) 
+        { 
+        }
+
+        public DbSet<Candidate>? Student { get; set; }
+    }
+}
---
+++ HireManagment/HireManagment.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using HireManagment.Application.Persistence.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+        
+            return services;
+        }
+    }
+}
