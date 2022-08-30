2c7c7dedf007349c2b9baefe380884eb3b291cae
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 11:09:23 -0600

    create opening class

---
+++ HireManagment/HireManagment.Domain/Opening.cs
@@ -0,0 +1,22 @@
+﻿using HireManagment.Domain.Common;
+using HireManagment.Domain.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Domain
+{
+    public class Opening : BaseDomainEntity
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateExpiration { get; set; }
+        public OpeningType OpeningType { get; set; }
+
+        public int EmployeeId { get; set; }
+        public virtual Employee? Employee { get; set; }
+    }
+}
