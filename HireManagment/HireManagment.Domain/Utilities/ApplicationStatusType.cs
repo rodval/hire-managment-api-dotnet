e503ac430ac90782c155ae571f9e00b40086337d
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 11:34:27 -0600

    Create ApplicationStatusType.cs

---
+++ HireManagment/HireManagment.Domain/Utilities/ApplicationStatusType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Domain.Utilities
+{
+    public enum ApplicationStatusType
+    {
+        InProcess = 1,
+        Accepted = 2,
+        Rejected = 3
+    }
+}
