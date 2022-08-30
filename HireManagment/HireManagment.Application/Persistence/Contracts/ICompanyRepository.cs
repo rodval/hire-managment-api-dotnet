e7a8d209498305f70f7b0aa73b440436778bd03f
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 14:25:48 -0600

    Update ICompanyRepository.cs

--- HireManagment/HireManagment.Application/Persistence/Contracts/ICompanyRepository.cs
+++ HireManagment/HireManagment.Application/Persistence/Contracts/ICompanyRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HireManagment.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HireManagment.Application.Persistence.Contracts
 {
-    internal interface ICompanyRepository
+    internal interface ICompanyRepository : IGenericRepository<Company>
     {
     }
 }
