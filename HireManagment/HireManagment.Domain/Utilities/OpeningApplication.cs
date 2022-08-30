f0e506d89b634ec49d7d4f5be9cd4f3a4c552053
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 11:34:44 -0600

    Create OpeningApplication.cs

---
+++ HireManagment/HireManagment.Domain/Utilities/OpeningApplication.cs
@@ -0,0 +1,21 @@
+﻿using HireManagment.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Domain.Utilities
+{
+    public class OpeningApplication : BaseDomainEntity
+    {
+        public DateTime DateApplication { get; set; }
+        public ApplicationStatusType Status { get; set; }
+
+        public int EmployeeId { get; set; }
+        public virtual Employee? Employee { get; set; }
+
+        public int CandidateId { get; set; }
+        public virtual Candidate? Candidate { get; set; }
+    }
+}
