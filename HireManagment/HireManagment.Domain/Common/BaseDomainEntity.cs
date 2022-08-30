952c86a52d8367d4fd725299c276fb3d845d1588
rodval <35208967+rodval@users.noreply.github.com>
2022-08-30 10:40:41 -0600

    crate base domian class

---
+++ HireManagment/HireManagment.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Domain.Common
+{
+    public class BaseDomainEntity
+    {
+        public int Id { get; set; }
+    }
+}
