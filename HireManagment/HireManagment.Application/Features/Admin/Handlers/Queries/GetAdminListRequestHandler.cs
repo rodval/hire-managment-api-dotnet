c14fa5a9705cb74c96c51ed3ec916ee325e8b824
rodval <35208967+rodval@users.noreply.github.com>
2022-09-02 09:54:37 -0600

    refactor classes

---
+++ HireManagment/HireManagment.Application/Features/Admin/Handlers/Queries/GetAdminListRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HireManagment.Application.Contracts.Persistence;
+using HireManagment.Application.DTOs.Admin;
+using HireManagment.Application.Features.Admin.Request.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.Features.Admin.Handlers.Queries
+{
+    internal class GetAdminListRequestHandler : IRequestHandler<GetAdminListRequest, List<AdminListDto>>
+    {
+        private readonly IAdminRepository _adminRepository;
+        private readonly IMapper _mapper;
+
+        public GetAdminListRequestHandler(IAdminRepository enrollmentRepository, IMapper mapper)
+        {
+            _adminRepository = enrollmentRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<AdminListDto>> Handle(GetAdminListRequest request, CancellationToken cancellationToken)
+        {
+            var admin = await _adminRepository.GetAll();
+            return _mapper.Map<List<AdminListDto>>(admin);
+        }
+    }
+}
---
+++ HireManagment/HireManagment.Domain/Entities/AdminConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Domain.Entities
+{
+    public class AdminConfiguration : IEntityTypeConfiguration<Admin>
+    {
+        public void Configure(EntityTypeBuilder<Admin> builder)
+        {
+            builder.HasData(
+                new Admin
+                {
+                    Id = 1,
+                    FirstName = "Robert",
+                    LastName = "Wade",
+                    Age = 32,
+                    Email = "rodrigovalladares1@gmail.com"
+                },
+                new Admin
+                {
+                    Id = 2,
+                    FirstName = "Felix",
+                    LastName = "Feliz",
+                    Age= 32,
+                    Email = "rodrigovalladares1@gmail.com"
+                }
+            );
+        }
+    }
+}
