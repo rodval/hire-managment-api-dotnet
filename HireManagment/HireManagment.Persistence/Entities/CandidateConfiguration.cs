c25e834da5c3b20d175cc8086cb4607ca075f92b
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 13:23:02 -0600

    Create CandidateConfiguration.cs

---
+++ HireManagment/HireManagment.Persistence/Entities/CandidateConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using HireManagment.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Persistence.Entities
+{
+    public class CandidateConfiguration : IEntityTypeConfiguration<Candidate>
+    {
+        public void Configure(EntityTypeBuilder<Candidate> builder)
+        {
+            builder.HasData(
+                new Candidate
+                {
+                    Id = 1,
+                    FirstName = "Omar",
+                    LastName = "Strange",
+                    Age = 32,
+                    Email = "rodrigovalladares1@gmail.com"
+                },
+                new Candidate
+                {
+                    Id = 2,
+                    FirstName = "Ruben",
+                    LastName = "Dario",
+                    Age = 32,
+                    Email = "rodrigovalladares1@gmail.com"
+                }
+            );
+        }
+    }
+}
