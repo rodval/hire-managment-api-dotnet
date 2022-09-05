e55bb88147f47e50b76ce13528a5002128f38c8b
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 12:24:16 -0600

    create opening method

---
+++ HireManagment/HireManagment.Application/Features/Openings/Request/Command/CreateOpeningCommand.cs
@@ -0,0 +1,17 @@
+﻿using HireManagment.Application.DTOs.Admin;
+using HireManagment.Application.DTOs.Opening;
+using HireManagment.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.Features.Openings.Request.Command
+{
+    public class CreateOpeningCommand : IRequest<BaseCommandResponses>
+    {
+        public CreateOpeningDto CreateOpening { get; set; }
+    }
+}
