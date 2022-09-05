9df176e0435beebc9238cc8f12942c88401de5ee
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 11:24:17 -0600

    create delete company employee method

---
+++ HireManagment/HireManagment.Application/Features/CompanyEmployees/Handler/Commands/DeleteCompanyEmployeeCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using HireManagment.Application.Contracts.Persistence;
+using HireManagment.Application.Exceptions;
+using HireManagment.Application.Features.Admins.Request.Commands;
+using HireManagment.Application.Features.CompanyEmployees.Request.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.Features.CompanyEmployees.Handler.Commands
+{
+    public class DeleteCompanyEmployeeCommandHandler : IRequestHandler<DeleteCompanyEmployeeCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public DeleteCompanyEmployeeCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteCompanyEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            var employee = await _unitOfWork.CompanyEmployeeRepository.Get(request.Id);
+
+            if (employee == null)
+                throw new NotFoundException(nameof(employee), request.Id);
+
+            await _unitOfWork.CompanyEmployeeRepository.Delete(employee);
+            await _unitOfWork.Save();
+
+            return Unit.Value;
+        }
+    }
+}
