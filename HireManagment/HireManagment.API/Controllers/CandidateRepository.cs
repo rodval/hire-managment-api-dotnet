83ea77cdeb7c9081b1c74b1f3ad7f9fda12f2e5f
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 13:47:59 -0600

    Create CandidateRepository.cs

---
+++ HireManagment/HireManagment.API/Controllers/CandidateRepository.cs
@@ -0,0 +1,70 @@
+﻿using HireManagment.Application.DTOs.Admin;
+using HireManagment.Application.DTOs.Candidate;
+using HireManagment.Application.Features.Admins.Request.Commands;
+using HireManagment.Application.Features.Admins.Request.Queries;
+using HireManagment.Application.Features.Candidates.Request.Commands;
+using HireManagment.Application.Features.Candidates.Request.Queries;
+using HireManagment.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HireManagment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateRepository : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public CandidateRepository(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CandidateListDto>>> Get()
+        {
+            var admins = await _mediator.Send(new GetCandidateListRequest());
+            return Ok(admins);
+        }
+
+        [HttpGet("{candidateId}")]
+        public async Task<ActionResult<CandidateDto>> Get(int candidateId)
+        {
+            var admin = await _mediator.Send(new GetCandidateRequest { CandidateId = candidateId });
+            return Ok(admin);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<BaseCommandResponses>> Post([FromBody] CreateCandidateDto candidate)
+        {
+            var command = new CreateCandidateCommand { CreateCandidate = candidate };
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpPut("{candidateId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Put([FromBody] UpdateCandidateDto candidate)
+        {
+            var command = new UpdateCandidateCommand { Candidate = candidate };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{candidateId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(int candidateId)
+        {
+            var command = new DeleteAdminCommand { Id = candidateId };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
