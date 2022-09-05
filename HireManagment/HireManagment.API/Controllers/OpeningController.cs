08c8b8e8bfb9d7c40294a1a3e83a2b4a95d244be
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 12:33:42 -0600

    Update OpeningController.cs

--- HireManagment/HireManagment.API/Controllers/OpeningController.cs
+++ HireManagment/HireManagment.API/Controllers/OpeningController.cs
@@ -1,4 +1,12 @@
-﻿using MediatR;
+﻿using HireManagment.Application.DTOs.Admin;
+using HireManagment.Application.DTOs.Opening;
+using HireManagment.Application.Features.Admins.Request.Commands;
+using HireManagment.Application.Features.Admins.Request.Queries;
+using HireManagment.Application.Features.Openings.Request.Command;
+using HireManagment.Application.Features.Openings.Request.Queries;
+using HireManagment.Application.Responses;
+using HireManagment.Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HireManagment.API.Controllers
@@ -12,6 +20,52 @@ namespace HireManagment.API.Controllers
         public OpeningController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<OpeningListDto>>> Get()
+        {
+            var admins = await _mediator.Send(new GetOpeningListRequest());
+            return Ok(admins);
+        }
+
+        [HttpGet("{openingId}")]
+        public async Task<ActionResult<OpeningDto>> Get(int openingId)
+        {
+            var admin = await _mediator.Send(new GetOpeningRequest { OpeningId = openingId });
+            return Ok(admin);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<BaseCommandResponses>> Post([FromBody] CreateOpeningDto opening)
+        {
+            var command = new CreateOpeningCommand { CreateOpening = opening };
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpPut("{openingId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Put([FromBody] UpdateOpeningDto opening)
+        {
+            var command = new UpdateOpeningCommand { Opening = opening };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{openingId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(int openingId)
+        {
+            var command = new DeleteOpeningCommand { Id = openingId };
+            await _mediator.Send(command);
+            return NoContent();
         }
     }
 }
