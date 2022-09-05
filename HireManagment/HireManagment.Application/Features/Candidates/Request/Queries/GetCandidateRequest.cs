dec60f62557ff444d33d570dd13e0a4f1e7de2b9
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 13:25:45 -0600

    Create GetCandidateRequest.cs

---
+++ HireManagment/HireManagment.Application/Features/Candidates/Request/Queries/GetCandidateRequest.cs
@@ -0,0 +1,16 @@
+﻿using HireManagment.Application.DTOs.Admin;
+using HireManagment.Application.DTOs.Candidate;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.Features.Candidates.Request.Queries
+{
+    public class GetCandidateRequest : IRequest<CandidateDto>
+    {
+        public int CandidateId { get; set; }
+    }
+}
