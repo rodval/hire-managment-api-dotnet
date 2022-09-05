d233737142f761b1ed1eda9bd9a01682549cee77
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 15:17:05 -0600

    Create OpeningApplicationRepository.cs

---
+++ HireManagment/HireManagment.Persistence/Repositories/OpeningApplicationRepository.cs
@@ -0,0 +1,21 @@
+﻿using HireManagment.Application.Contracts.Persistence;
+using HireManagment.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Persistence.Repositories
+{
+    public class OpeningApplicationRepository : GenericRepository<OpeningApplication>, IOpeningApplicationRepository
+    {
+        private readonly HireManagmentDbContext _dbContext;
+
+        public OpeningApplicationRepository(HireManagmentDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+    }
+}
