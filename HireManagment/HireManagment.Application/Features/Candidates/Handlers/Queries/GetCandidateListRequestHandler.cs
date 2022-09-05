e85963e808146c5a9535ab0eda92e669162e8d8e
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 13:36:04 -0600

    get all candidates method

---
+++ HireManagment/HireManagment.Application/Features/Candidates/Handlers/Queries/GetCandidateListRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HireManagment.Application.Contracts.Persistence;
+using HireManagment.Application.DTOs.Admin;
+using HireManagment.Application.DTOs.Candidate;
+using HireManagment.Application.Features.Admins.Request.Queries;
+using HireManagment.Application.Features.Candidates.Request.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.Features.Candidates.Handlers.Queries
+{
+    public class GetCandidateListRequestHandler : IRequestHandler<GetCandidateListRequest, List<CandidateListDto>>
+    {
+        private readonly ICandidateRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetCandidateListRequestHandler(ICandidateRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CandidateListDto>> Handle(GetCandidateListRequest request, CancellationToken cancellationToken)
+        {
+            var admin = await _repository.GetAll();
+            return _mapper.Map<List<CandidateListDto>>(admin);
+        }
+    }
+}
---
+++ HireManagment/HireManagment.Application/Features/Candidates/Handlers/Queries/GetCandidateRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HireManagment.Application.Contracts.Persistence;
+using HireManagment.Application.DTOs.Admin;
+using HireManagment.Application.DTOs.Candidate;
+using HireManagment.Application.Features.Admins.Request.Queries;
+using HireManagment.Application.Features.Candidates.Request.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.Features.Candidates.Handlers.Queries
+{
+    public class GetCandidateRequestHandler : IRequestHandler<GetCandidateRequest, CandidateDto>
+    {
+        private readonly ICandidateRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetCandidateRequestHandler(ICandidateRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<CandidateDto> Handle(GetCandidateRequest request, CancellationToken cancellationToken)
+        {
+            var candidate = await _repository.Get(request.CandidateId);
+            return _mapper.Map<CandidateDto>(candidate);
+        }
+    }
+}
