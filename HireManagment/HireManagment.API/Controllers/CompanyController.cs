e5609f136f4470678f69576189df75cb263d46e3
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 10:02:03 -0600

    Update CompanyController.cs

--- HireManagment/HireManagment.API/Controllers/CompanyController.cs
+++ HireManagment/HireManagment.API/Controllers/CompanyController.cs
@@ -1,5 +1,6 @@
 ﻿using HireManagment.Application.DTOs.Admin;
 using HireManagment.Application.DTOs.Company;
+using HireManagment.Application.Features.Admins.Request.Commands;
 using HireManagment.Application.Features.Companies.Request.Commands;
 using HireManagment.Application.Features.Companies.Request.Queries;
 using HireManagment.Application.Responses;
@@ -41,6 +42,28 @@ namespace HireManagment.API.Controllers
             var command = new CreateCompanyCommand { CreateCompany = company };
             var response = await _mediator.Send(command);
             return Ok(response);
+        }
+
+        [HttpPut("{adminId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Put([FromBody] UpdateCompanyDto company)
+        {
+            var command = new UpdateCompanyCommand { Company = company };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{adminId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(int companyId)
+        {
+            var command = new DeleteCompanyCommand { Id = companyId };
+            await _mediator.Send(command);
+            return NoContent();
         }
     }
 }
