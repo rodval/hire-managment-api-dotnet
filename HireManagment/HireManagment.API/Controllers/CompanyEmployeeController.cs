69062c23adc3b2cb54de6596e05e775cd9e59911
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 10:24:55 -0600

    Create CompanyEmployeeController.cs

---
+++ HireManagment/HireManagment.API/Controllers/CompanyEmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using HireManagment.Application.DTOs.Admin;
+using HireManagment.Application.DTOs.CompanyEmployee;
+using HireManagment.Application.Features.Admins.Request.Queries;
+using HireManagment.Application.Features.CompanyEmployees.Request.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HireManagment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyEmployeeController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public CompanyEmployeeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /*[HttpGet]
+        public async Task<ActionResult<List<AdminApiListDto>>> Get()
+        {
+            var admins = await _mediator.Send(new GetAdminListRequest());
+            return Ok(admins);
+        }*/
+
+        [HttpGet("{employeeId}")]
+        public async Task<ActionResult<CompanyEmployeeDto>> Get(int employeeId)
+        {
+            var employee = await _mediator.Send(new GetCompanyEmployeeRequest { CompanyEmployeeId = employeeId });
+            return Ok(employee);
+        }
+    }
+}
