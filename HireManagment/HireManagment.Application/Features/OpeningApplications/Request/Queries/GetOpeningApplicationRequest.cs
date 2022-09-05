d2fd91fe0312baf72cac14940bc090dfa68008c0
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 15:17:40 -0600

    get opening application method

---
+++ HireManagment/HireManagment.Application/Features/OpeningApplications/Request/Queries/GetOpeningApplicationRequest.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HireManagment.Application.Contracts.Persistence;
+using HireManagment.Application.DTOs.OpeningApplication;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.Features.OpeningApplications.Request.Queries
+{
+    public class GetOpeningApplicationRequest : IRequest<OpeningApplicationDto>
+    {
+        public int OpeningApplicationId { get; set; }
+    }
+}
