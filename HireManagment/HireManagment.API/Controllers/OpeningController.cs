3ce80bafeb359f5c80d6c1f2860eb7542e17afe3
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-05 11:53:59 -0600

    Create OpeningController.cs

---
+++ HireManagment/HireManagment.API/Controllers/OpeningController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HireManagment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpeningController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public OpeningController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
