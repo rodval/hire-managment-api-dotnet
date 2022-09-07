583f1f9a6e4ea9c7397f255e668bdc9d94500806
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-07 08:45:17 -0600

    create auth entities

---
+++ HireManagment/HireManagment.Application/Models/Identity/JwtSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.Models.Identity
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; }
+        public double DurationInMinutes { get; set; }
+    }
+}
