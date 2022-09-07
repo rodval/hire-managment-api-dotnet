0cf94a101c1e86fd2a8a686a6860ebea5bca91f7
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-07 08:45:55 -0600

    Update Person.cs

--- HireManagment/HireManagment.Domain/Common/Person.cs
+++ HireManagment/HireManagment.Domain/Common/Person.cs
@@ -12,5 +12,7 @@ namespace HireManagment.Domain.Common
         public string? LastName { get; set; }
         public int? Age { get; set; }
         public string? Email { get; set; }
+
+        public string? Password { get; set; }
     }
 }
