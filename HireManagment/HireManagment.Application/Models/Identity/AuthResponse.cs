0daf05b1d7bafdad8ad7ec1d8db7ec358dc1d052
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-07 10:03:25 -0600

    Update AuthResponse.cs

--- HireManagment/HireManagment.Application/Models/Identity/AuthResponse.cs
+++ HireManagment/HireManagment.Application/Models/Identity/AuthResponse.cs
@@ -9,6 +9,7 @@ namespace HireManagment.Application.Models.Identity
     public class AuthResponse
     {
         public int Id { get; set; }
+        public string? Email { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Token { get; set; }
