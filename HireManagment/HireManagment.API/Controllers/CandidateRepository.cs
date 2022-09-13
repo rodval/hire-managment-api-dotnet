3d6389e6d556c01fcb5d7b197ee411004e40e9b7
rodval <35208967+rodval@users.noreply.github.com>
2022-09-13 11:54:19 -0600

    Update CandidateRepository.cs

--- HireManagment/HireManagment.API/Controllers/CandidateRepository.cs
+++ HireManagment/HireManagment.API/Controllers/CandidateRepository.cs
@@ -50,7 +50,6 @@ namespace HireManagment.API.Controllers
             return Ok(response);
         }
 
-        [HttpPut("{candidateId}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesDefaultResponseType]
