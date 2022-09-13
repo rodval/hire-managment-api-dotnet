a15e99aa2635a0509005a0ed3080596f3c52e854
rodval <35208967+rodval@users.noreply.github.com>
2022-09-13 11:55:40 -0600

    create getcandidatesopening method

---
+++ HireManagment/HireManagment.Application/Features/OpeningApplications/Request/Queries/GetCandidatesaApplicationListRequest.cs
@@ -0,0 +1,15 @@
+﻿using HireManagment.Application.DTOs.OpeningApplication;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Application.Features.OpeningApplications.Request.Queries
+{
+    public class GetCandidatesaApplicationListRequest : IRequest<List<OpeningApplicationListDto>>
+    {
+        public string CandidateId { get; set; }
+    }
+}
