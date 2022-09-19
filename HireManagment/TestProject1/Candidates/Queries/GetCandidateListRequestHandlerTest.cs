ffc760d85d94d1b2b62de07ecb585a6eaa511458
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-19 09:46:59 -0600

    get candidates test

---
+++ HireManagment/TestProject1/Candidates/Queries/GetCandidateListRequestHandlerTest.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using HireManagment.Application.Contracts.Persistence;
+using HireManagment.Application.DTOs.Candidate;
+using HireManagment.Application.Features.Candidates.Handlers.Queries;
+using HireManagment.Application.Features.Candidates.Request.Queries;
+using HireManagment.Application.Profiles;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject1.Mocks;
+
+namespace TestProject1.Candidates.Queries
+{
+    public class GetCandidateListRequestHandlerTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ICandidateRepository> _mockRepo;
+        public GetCandidateListRequestHandlerTest()
+        {
+            _mockRepo = MockCandidateRepository.GetCandidateRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListTest()
+        {
+            var handler = new GetCandidateListRequestHandler(_mockRepo.Object, _mapper);
+
+            var result = await handler.Handle(new GetCandidateListRequest(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<CandidateListDto>>();
+
+            result.Count.ShouldBe(2);
+        }
+    }
+}
---
+++ HireManagment/TestProject1/Mocks/MockUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using HireManagment.Application.Contracts.Persistence;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.Mocks
+{
+    public static class MockUnitOfWork
+    {
+        public static Mock<IUnitOfWork> GetUnitOfWork()
+        {
+            var mockUow = new Mock<IUnitOfWork>();
+            var mockCandidateRepo = MockCandidateRepository.GetCandidateRepository();
+
+            mockUow.Setup(r => r.CandidateRepository).Returns(mockCandidateRepo.Object);
+
+            return mockUow;
+        }
+    }
+}
