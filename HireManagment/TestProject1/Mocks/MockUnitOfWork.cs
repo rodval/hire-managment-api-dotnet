43fed03f3c7e23a50364c2ad19dae45474c9ce78
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-20 08:21:36 -0600

    Update MockUnitOfWork.cs

--- HireManagment/TestProject1/Mocks/MockUnitOfWork.cs
+++ HireManagment/TestProject1/Mocks/MockUnitOfWork.cs
@@ -13,9 +13,12 @@ namespace HireManagment.Test.Mocks
         public static Mock<IUnitOfWork> GetUnitOfWork()
         {
             var mockUow = new Mock<IUnitOfWork>();
+
             var mockCandidateRepo = MockCandidateRepository.GetCandidateRepository();
+            var mockAdminRepo = MockAdminRepository.GetAdminRepository();
 
             mockUow.Setup(r => r.CandidateRepository).Returns(mockCandidateRepo.Object);
+            mockUow.Setup(r => r.AdminRepository).Returns(mockAdminRepo.Object);
 
             return mockUow;
         }
