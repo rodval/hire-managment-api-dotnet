a964929e1f07fd3afac6acbf1649f6f8d6e629a1
roDvalM <35208967+rodval@users.noreply.github.com>
2022-09-20 11:23:46 -0600

    Create GetCompanyListRequestHandlerTest.cs

---
+++ HireManagment/TestProject1/Companies/Queries/GetCompanyListRequestHandlerTest.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using HireManagment.Application.Contracts.Persistence;
+using HireManagment.Application.DTOs.Company;
+using HireManagment.Application.Features.Companies.Handlers.Queries;
+using HireManagment.Application.Features.Companies.Request.Queries;
+using HireManagment.Application.Profiles;
+using HireManagment.Test.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireManagment.Test.Companies.Queries
+{
+    public class GetCompanyListRequestHandlerTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ICompanyRepository> _mockRepo;
+        public GetCompanyListRequestHandlerTest()
+        {
+            _mockRepo = MockCompanyRepository.GetCompanyRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GeCandidateListTest()
+        {
+            var handler = new GetCompanyListRequestHandler(_mockRepo.Object, _mapper);
+
+            var result = await handler.Handle(new GetCompanyListRequest(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<CompanyListDto>>();
+
+            result.Count.ShouldBe(2);
+        }
+    }
+}
